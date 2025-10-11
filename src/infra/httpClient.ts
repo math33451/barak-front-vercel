67b3a5132b62e58dad2950e2c94b55a37cc8c30f
Matheus Bezerra <matheus.bezerra@aluno.faculdadeimpacta.com.br>
2025-10-10 21:49:16 -0300

    [feat]: define url backend como variavel de ambiente

--- src/infra/httpClient.ts
+++ src/infra/httpClient.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8089";
+const API_BASE_URL =  process.env.NEXT_PUBLIC_API_URL;
 
 // Função para pegar o token
 const getToken = () => {
